2f5815501c7c56802c043ea35f8612f65369b4d2
NPierce1798 <npierce1798@gmail.com>
2024-08-26 17:15:51 -0400

    eslint

--- src/components/api.js
+++ src/components/api.js
@@ -1,26 +1,25 @@
 // api.js
+
 import axios from 'axios';
 
-
-
-
-const API_BASE_URL = `http://165.227.86.141:8000/api/`; // Update this URL to match your Django endpoint
+const API_BASE_URL = 'http://165.227.86.141:8000/api/'; // Update this URL to match your Django endpoint
 
 // Fetch existing posts
 export const getPosts = () => {
-    return axios.get(`${API_BASE_URL}posts/`);
+  return axios.get(`${API_BASE_URL}posts/`);
 };
 
 // Send a new post (JSON object) to the Django backend
 export const createPost = (postData) => {
   console.log('Sending data to Django:', postData); // Debugging output
-    return axios.post(`${API_BASE_URL}session/`, postData, {
+  return axios.post(`${API_BASE_URL}session/`, postData, {
     headers: {
       'Content-Type': 'application/json', // Ensure correct content type
     },
-    });
+  });
 };
 
+// Continue the story with new data
 export const continueStory = (data) => {
-    return axios.post(`${API_BASE_URL}session/`, data);
+  return axios.post(`${API_BASE_URL}session/`, data);
 };
