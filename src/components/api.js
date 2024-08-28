a68f17d8ae932da5b6ac23d2b46625d00d53304d
NPierce1798 <npierce1798@gmail.com>
2024-08-27 22:43:32 -0400

    Subdomain update

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://165.227.86.141:8000/api/'; // Ensure this is defined correctly
+const API_BASE_URL = 'https://api.nicolaspierce.com/api/'; // Ensure this is defined correctly
 
 // Fetch existing posts
 export const getPosts = () => {
